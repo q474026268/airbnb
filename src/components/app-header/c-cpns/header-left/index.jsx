cc7fb4be61f07040ebeef1938f8330a40211a768
ye_te <474026268@qq.com>
2023-11-09 11:37:12 +0800

    update：项目-Logo的svg图片的两种使用方式

--- src/components/app-header/c-cpns/header-left/index.jsx
+++ src/components/app-header/c-cpns/header-left/index.jsx
@@ -1,10 +1,11 @@
 import React, { memo } from "react";
 import { LeftWrapper } from "./style";
+import logo from "@/assets/img/logo.png";
 
 export default memo(() => {
   return (
     <LeftWrapper>
-      HeaderLeft
+      <img src={logo} alt="" />
     </LeftWrapper>
   );
 });
